37272afc2de937ce3912ba8f1048cbb6cdc9d8c8
gpokhark <gpokhark@gmail.com>
2019-10-31 23:30:46 -0400

    2_13_Store_Grid and added .gitignore for c++ and cmake.

---
+++ 2_13_Store_Grid/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<std::vector<int>> board = {{0, 1, 0, 0, 0, 0},
+                                           {0, 1, 0, 0, 0, 0},
+                                           {0, 1, 0, 0, 0, 0},
+                                           {0, 1, 0, 0, 0, 0},
+                                           {0, 0, 0, 0, 1, 0}};
+
+    std::cout << "Hello!" << "\n";
+}
