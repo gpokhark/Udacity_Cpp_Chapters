4cf3e6c8bb1328a2dbdae12a63be3c1bd4f082c2
gpokhark <gpokhark@gmail.com>
2019-11-17 16:52:25 -0500

    Static member -> const, static and constexpr.

---
+++ 3_Object_Oriented_Programming/2_25_Static_Members/main1.cpp
@@ -0,0 +1,57 @@
+#include <cassert>
+#include <cmath>
+#include <stdexcept>
+
+class Sphere
+{
+public:
+    Sphere(int radius) : radius_(radius), volume_(pi_ * 4 / 3 * pow(radius_, 3))
+    {
+        if (radius <= 0)
+            throw std::invalid_argument("radius must be positive");
+    }
+
+    int Radius() const { return radius_; }
+    int Volume() const { return volume_; }
+
+    // TODO: mutator
+    void Radius(int radius)
+    {
+        if (radius <= 0)
+            throw std::invalid_argument("radius must be positive");
+        radius_ = radius;
+        volume_ = pi_ * 4 / 3 * pow(radius_, 3);
+    }
+
+    static float const pi_;
+
+private:
+    int radius_;
+    float volume_;
+};
+
+// Initializing it outside of the class
+float const Sphere::pi_{3.14159};
+
+// Test
+int main(void)
+{
+    Sphere sphere(5);
+    assert(sphere.Radius() == 5);
+    assert(abs(sphere.Volume() - 523.6) < 1);
+
+    sphere.Radius(3);
+    assert(sphere.Radius() == 3);
+    assert(abs(sphere.Volume() - 113.1) < 1);
+
+    bool caught{false};
+    try
+    {
+        sphere.Radius(-1);
+    }
+    catch (...)
+    {
+        caught = true;
+    }
+    assert(caught);
+}
