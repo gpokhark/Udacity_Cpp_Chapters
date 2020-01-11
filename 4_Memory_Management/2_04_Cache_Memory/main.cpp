87b183f72e60cf78fde85756f1663449f5b416cc
gpokhark <gpokhark@gmail.com>
2020-01-11 12:50:12 -0500

    Cache memory speed up, speed down demonstration on the way data is stored into an array.

--- 4_Memory_Management/2_04_Cache_Memory/main.cpp
+++ 4_Memory_Management/2_04_Cache_Memory/main.cpp
@@ -1,26 +1,38 @@
 #include <chrono>
 #include <iostream>
 
-int main()
-{
-    // create array
-    const int size = 10000;
-    static int x[size][size];
+int main() {
+  // create array
+  const int size = 10000;
+  static int x[size][size];
 
-    auto t1 = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            x[i][j] = i + j;
-            // std::cout << &x[j][i] << ": i=" << i << ", j=" << j << std::endl;
-        }
+  auto t1 = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      x[i][j] = i + j;
+      // std::cout << &x[j][i] << ": i=" << i << ", j=" << j << std::endl;
     }
+  }
 
-    // print execution time to console
-    auto t2 = std::chrono::high_resolution_clock::now(); // stop time measurement
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-    std::cout << "Execution time: " << duration << " microseconds" << std::endl;
+  // print execution time to console
+  auto t2 = std::chrono::high_resolution_clock::now();  // stop time measurement
+  auto duration =
+      std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+  std::cout << "Execution time: " << duration << " microseconds" << std::endl;
 
-    return 0;
+  auto t3 = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      x[j][i] = i + j;
+      // std::cout << &x[j][i] << ": i=" << i << ", j=" << j << std::endl;
+    }
+  }
+
+  // print execution time to console
+  auto t4 = std::chrono::high_resolution_clock::now();  // stop time measurement
+  duration =
+      std::chrono::duration_cast<std::chrono::microseconds>(t4 - t3).count();
+  std::cout << "Execution time: " << duration << " microseconds" << std::endl;
+
+  return 0;
 }
