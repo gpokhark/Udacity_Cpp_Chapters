2ab87c993aa327b3f155baf9dc2a9b61ee40073d
gpokhark <gpokhark@gmail.com>
2020-01-19 14:27:30 -0500

    Malloc vs New.

---
+++ 4_Memory_Management/4_04_New_and_Delete/malloc_vs_new.cpp
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <iostream>
+
+class MyClass {
+ private:
+  int *_number;
+
+ public:
+  MyClass() {
+    std::cout << "Allocate memory\n";
+    _number = (int *)malloc(sizeof(int));
+  }
+  ~MyClass() {
+    std::cout << "Delete memory\n";
+    free(_number);
+  }
+  void setNumber(int number) {
+    *_number = number;
+    std::cout << "Number: " << _number << "\n";
+  }
+};
+
+int main() {
+  //   // allocate memory using malloc
+  //   // comment these lines out to run the example below
+  //   MyClass *myClass = (MyClass *)malloc(sizeof(MyClass));
+  //   myClass->setNumber(42);  // EXC_BAD_ACCESS
+  //   free(myClass);
+
+  // allocate memory using new
+  MyClass *myClass = new MyClass();
+  myClass->setNumber(42);  // works as expected
+  delete myClass;
+
+  return 0;
+}
