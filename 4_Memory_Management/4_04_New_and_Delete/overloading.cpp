72c7673460a53f4b56b8a7694f623bc3ded751a9
gpokhark <gpokhark@gmail.com>
2020-01-19 15:31:37 -0500

    Overloading new and delete operators.

---
+++ 4_Memory_Management/4_04_New_and_Delete/overloading.cpp
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <iostream>
+
+class MyClass {
+  int _number;
+
+ public:
+  MyClass() { std::cout << "Constructor is called. \n"; }
+
+  ~MyClass() { std::cout << "Destructor is called. \n"; }
+
+  void* operator new(size_t size) {
+    std::cout << "new: Allocating " << size << " bytes of memory. \n";
+    void* p = malloc(size);
+    return p;
+  }
+
+  void operator delete(void* p) {
+    std::cout << "delete: Memory is freed again. \n";
+    free(p);
+  }
+};
+
+int main() {
+  MyClass* p = new MyClass();
+  delete p;
+  return 0;
+}
