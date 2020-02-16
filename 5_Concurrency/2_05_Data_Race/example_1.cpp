3bee4ab2a124c9f6e2ffd9a34e0115fc442fcdaa
gpokhark <gpokhark@gmail.com>
2020-02-15 20:05:11 -0500

    Passing data to a thread by value.

---
+++ 5_Concurrency/2_05_Data_Race/example_1.cpp
@@ -0,0 +1,44 @@
+#include <future>
+#include <iostream>
+#include <thread>
+
+class Vehicle {
+ private:
+  int _id;
+
+ public:
+  // default constructor
+  Vehicle() : _id(0) {
+    std::cout << "Vehicle #" << _id << " Default constructor called.\n";
+  }
+
+  // initializing constructor
+  Vehicle(int id) : _id(id) {
+    std::cout << "Vehicle #" << _id << " Initializing constructor called.\n";
+  }
+
+  // setter and getter
+  void setID(int id) { _id = id; }
+  int getID() { return _id; }
+};
+
+int main() {
+  // create instances of class Vehicle
+  Vehicle v0;     // default constructor
+  Vehicle v1(1);  // initializing constructor
+
+  // read and write name in different threads (which one of the above creates a
+  // data race?)
+  // lambda function
+  std::future<void> ftr = std::async(
+      [](Vehicle v) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        v.setID(2);
+      },
+      v0);
+  v0.setID(3);
+  ftr.wait();
+
+  std::cout << "Vehicle #" << v0.getID() << std::endl;
+  return 0;
+}
