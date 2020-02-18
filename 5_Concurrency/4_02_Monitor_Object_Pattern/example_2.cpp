3fd7534d293b794b16b514b877dd673c1d00ccf0
gpokhark <gpokhark@gmail.com>
2020-02-17 22:54:28 -0500

    Vehicle counter to exit the infinite polling loop.

--- 5_Concurrency/4_02_Monitor_Object_Pattern/example_2.cpp
+++ 5_Concurrency/4_02_Monitor_Object_Pattern/example_2.cpp
@@ -19,9 +19,15 @@ class WaitingVehicles {
   std::vector<Vehicle>
       _vehicles;  // list of all vehicles waiting to enter this intersection
   std::mutex _mutex;
+  int _numVehicles;
 
  public:
-  WaitingVehicles() {}
+  WaitingVehicles() : _numVehicles(0) {}
+
+  int getNumVehicles() {
+    std::lock_guard<std::mutex> uLock(_mutex);
+    return _numVehicles;
+  }
 
   bool dataIsAvailable() {
     std::lock_guard<std::mutex> myLock(_mutex);
@@ -32,11 +38,12 @@ class WaitingVehicles {
     // perform vector modification under the lock
     std::lock_guard<std::mutex> uLock(_mutex);
 
-    // remove last vector element from the queue
+    // remove last vector element from queue
     Vehicle v = std::move(_vehicles.back());
     _vehicles.pop_back();
+    --_numVehicles;
 
-    // will not be copied due to return value optimization (ROV) in c++
+    // will not be copied due to return value optimization (RVO) in c++
     return v;
   }
 
@@ -51,6 +58,7 @@ class WaitingVehicles {
     std::cout << "Vehicle #" << v.getID() << " will be added to the queue.\n";
 
     _vehicles.emplace_back(std::move(v));
+    ++_numVehicles;
   }
 };
 
@@ -61,7 +69,8 @@ int main() {
 
   std::cout << "Spawning threads...\n";
   std::vector<std::future<void>> futures;
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 10; ++i) {
+    // create a new Vehicle instance and move it into the queue
     Vehicle v(i);
     futures.emplace_back(std::async(
         std::launch::async, &WaitingVehicles::pushBack, queue, std::move(v)));
@@ -74,12 +83,19 @@ int main() {
       Vehicle v = queue->popBack();
       std::cout << " Vehicle #" << v.getID()
                 << " has been removed from the queue" << std::endl;
+
+      if (queue->getNumVehicles() <= 0) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        break;
+      }
     }
   }
 
   std::for_each(futures.begin(), futures.end(),
                 [](std::future<void> &ftr) { ftr.wait(); });
 
-  std::cout << "Finished processing queue.\n";
+  std::cout << "Finished : " << queue->getNumVehicles()
+            << " vehicle(s) left in the queue" << std::endl;
+
   return 0;
 }
