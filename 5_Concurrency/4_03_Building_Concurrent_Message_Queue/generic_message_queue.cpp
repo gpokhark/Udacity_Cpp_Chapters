17ee3929e4e1f6f05d219999152719909628694e
gpokhark <gpokhark@gmail.com>
2020-02-18 23:39:15 -0500

    Message counter so that the program can terminate.

--- 5_Concurrency/4_03_Building_Concurrent_Message_Queue/generic_message_queue.cpp
+++ 5_Concurrency/4_03_Building_Concurrent_Message_Queue/generic_message_queue.cpp
@@ -9,6 +9,13 @@
 template <class T>
 class MessageQueue {
  public:
+  MessageQueue() : _numMessages(0) {}
+
+  int getNumMessages() {
+    std::lock_guard<std::mutex> uLock(_mutex);
+    return _numMessages;
+  }
+
   T receive() {
     // perform queue modification under the lock
     std::unique_lock<std::mutex> uLock(_mutex);
@@ -18,7 +25,7 @@ class MessageQueue {
     // remove last vector element from queue
     T msg = std::move(_messages.back());
     _messages.pop_back();
-
+    --_numMessages;
     // will not be copied due to Return Value Optimization (RVO) in c++
     return msg;
   }
@@ -34,7 +41,7 @@ class MessageQueue {
     std::cout << "   Message " << msg << " has been sent to the queue.\n";
 
     _messages.push_back(std::move(msg));
-
+    ++_numMessages;
     // notify client after pushing new message into the vector
     _cond.notify_one();
   }
@@ -43,6 +50,7 @@ class MessageQueue {
   std::mutex _mutex;
   std::condition_variable _cond;
   std::deque<T> _messages;
+  int _numMessages;
 };
 
 int main() {
@@ -65,6 +73,11 @@ int main() {
     int message = queue->receive();
     std::cout << "    Message #" << message
               << " has been removed form the queue.\n";
+
+    if (queue->getNumMessages() <= 0) {
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+      break;
+    }
   }
 
   std::for_each(futures.begin(), futures.end(),
